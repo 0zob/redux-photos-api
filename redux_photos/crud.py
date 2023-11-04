93a9d06e2be6eb1fb6c0fffe8ca170d8690b1c2a
enzotorrs <enzotorres969@gmail.com>
2023-11-04 12:45:54 -0300

    feat: add crud utils for User

--- redux_photos/crud.py
+++ redux_photos/crud.py
@@ -5,9 +5,19 @@ from . import models, schemas
 def get_photos(db: Session):
     return db.query(models.Photo).all()
 
-def create_photo(db: Session, photo: schemas.PhotoCreate):
-    db_photo = models.Photo(title=photo.title, like=photo.like, img_src=photo.img_src)
+def create_user_photo(db: Session, photo: schemas.PhotoCreate, user_id: int):
+    db_photo = models.Photo(**photo.model_dump(), owner_id=user_id)
     db.add(db_photo)
     db.commit()
     db.refresh(db_photo)
     return db_photo
+
+def get_users(db: Session):
+    return db.query(models.User).all()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(name=user.name, icon_src=user.icon_src)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
