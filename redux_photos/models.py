4b7141178aafc409649d8691fd99e7b325ff4b00
enzotorrs <enzotorres969@gmail.com>
2023-11-04 12:45:03 -0300

    feat: add User model and relationship with photos

--- redux_photos/models.py
+++ redux_photos/models.py
@@ -1,4 +1,6 @@
 from sqlalchemy import  Boolean, Column,  Integer, String, Text
+from sqlalchemy.orm import Relationship
+from sqlalchemy.orm.properties import ForeignKey
 
 from .database import Base
 
@@ -9,3 +11,13 @@ class Photo(Base):
     title = Column(String(244))
     like =  Column(Boolean, default=False)
     img_src = Column(Text)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = Relationship("User", back_populates="photos")
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(244), index=True)
+    icon_src = Column(Text)
+    photos = Relationship("Photo", back_populates="owner")
+    
